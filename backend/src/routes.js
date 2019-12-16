8b88c1d4b0c5a7e101e4a7651b5fac4bd220da5a
Rafael Costa <rafaelcostat@gmail.com>
2019-12-16 09:41:56 -0300

    user authentication

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,26 @@
 import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+import StudentController from './app/controllers/StudentController';
+
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello...' });
+routes.get('/', async (req, res) => {
+  return res.json({ message: 'It Works!' });
 });
+
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+routes.put('/users', UserController.update);
+
+routes.post('/students', StudentController.store);
+routes.put('/students/:id', StudentController.update);
+routes.get('/students', StudentController.index);
+routes.delete('/students/:id', StudentController.delete);
 
 export default routes;
