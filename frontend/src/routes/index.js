96f8ac368f865c3a8347d473a0148e33438b80fe
Rafael Costa <rafaelcostat@gmail.com>
2019-12-18 11:05:18 -0300

    react routes config

---
+++ frontend/src/routes/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import SignIn from '../pages/SignIn';
+import Students from '../pages/Students';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+      <Route path="/students" component={Students} />
+
+      <Route path="/" component={() => <h1>404</h1>} />
+    </Switch>
+  );
+}
