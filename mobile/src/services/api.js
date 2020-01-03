8f21e0fd5ca73a0a37b373ad7ab4e9954912e3e0
Rafael Costa <rafaelcostat@gmail.com>
2020-01-03 09:21:44 -0300

    create docker compose file

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  // iOs
+  // baseURL: 'http://localhost:3333',
+
   // Android
-  // baseURL: 'http://192.168.25.5:3333',
+  baseURL: 'http://192.168.25.5:3333',
 });
 
 export default api;
